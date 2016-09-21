243baefcccc2af80255d879e066ecf255f064a57
Yusuke Yasuda <yusuke.007.yasud@gmail.com>
2016-09-21 13:38:17 +0900

    remove unused old akka stream version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,6 @@ import sbt._
 object Dependencies {
   val aws_sdk_version = "1.11.31"
   val akka_version = "2.4.9"
-  val akka_stream_version = "2.0.2"
   val scalatest_version = "2.2.6"
   val awssdk_core = "com.amazonaws" % "aws-java-sdk-core" % aws_sdk_version % "compile"
   val awssdk_elb = "com.amazonaws" % "aws-java-sdk-elasticloadbalancing" % aws_sdk_version % "compile"
